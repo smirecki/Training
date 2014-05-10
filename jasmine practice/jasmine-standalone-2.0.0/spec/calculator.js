c5d3aa27dc82765842b9bacd394ba59b6dca3c63
Steven Mirecki <steven.mirecki@gmail.com>
2014-05-09 21:39:28 -0400

    more

--- jasmine practice/jasmine-standalone-2.0.0/spec/calculator.js
+++ jasmine practice/jasmine-standalone-2.0.0/spec/calculator.js
@@ -1,7 +1,13 @@
 window.Calculator = {
-	current: 0,
+  current: 0,
 
-  add: function(num1) {
-    return this.current + num1;
+  add: function() {
+  	var sum = this.current;
+
+  	for (var i = 0; len = arguments.length; i<len; i++){
+        sum += arguments[i];
+  	} 
+    this.current = sum;
+    return this.current;
   }
 };
--- jasmine practice/jasmine-standalone-2.0.0/spec/calculator_spec.js
+++ jasmine practice/jasmine-standalone-2.0.0/spec/calculator_spec.js
@@ -1,10 +1,23 @@
 describe("Calculator", function() {
-	it("should store the current value at all times", function() {
-		expect(Calculator.current).toBeDefined();
-		expect(Calculator.current).toEqual(0);
+  beforeEach(function() {
+	Calculator.current = 0;
+  });
+
+  describe("When adding numbers", function() {
+    it("should store the current value at all times", function() {
+	  expect(Calculator.current).toBeDefined();
+	  expect(Calculator.current).toEqual(0);
 	});
+
 	it("should add numbers", function() {
-	   expect(Calculator.add(5)).toEqual(5);
-	   expect(Calculator.add(5)).toEqual(10);
+	  expect(Calculator.add(5)).toEqual(5);
+	  expect(Calculator.add(5)).toEqual(10);
 	});
+
+	it("should add any numbers", function() {
+      expect(Calculator.add(1,2,3)).toEqual(6);
+      expect(Calculator.add(1,2,3,4)).toEqual(16);
+	});
+  });
+
 });
