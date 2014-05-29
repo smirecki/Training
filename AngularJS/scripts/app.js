86badd61eb3de7205665048dcb245bad515f9864
Steven Mirecki <steven.mirecki@gmail.com>
2014-05-29 12:15:28 -0400

    Update 2.4 UI changes and image gallery controller/directives.

--- AngularJS/scripts/app.js
+++ AngularJS/scripts/app.js
@@ -16,6 +16,13 @@
 		};
 	});
 
+	app.controller('GalleryController', function(){
+		this.current = 0;
+		this.setCurrent = function(newGallery){
+			this.current = newGallery || 0;
+		};
+	});
+
   var gems = [
     {
       name: 'Azurite',
