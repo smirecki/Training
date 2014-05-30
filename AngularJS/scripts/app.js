6e08a43adce098daa64bb2b7f387ce6d16d382eb
Steven Mirecki <steven.mirecki@gmail.com>
2014-05-30 11:09:26 -0400

    Update 3.1 Form validation with ng

--- AngularJS/scripts/app.js
+++ AngularJS/scripts/app.js
@@ -1,30 +1,41 @@
 (function() {
-	var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', []);
 
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
 
-	app.controller("TabController", function(){
-		this.tab = 1;
+  app.controller("TabController", function() {
+    this.tab = 1;
 
-		this.setTab = function(newValue) {
-			this.tab = newValue;
-		};
-		this.isSet = function(tabName){
-			return this.tab === tabName;
-		};
-	});
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
 
-	app.controller('GalleryController', function(){
-		this.current = 0;
-		this.setCurrent = function(newGallery){
-			this.current = newGallery || 0;
-		};
-	});
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+  });
 
-  var gems = [
-    {
+  app.controller('GalleryController', function(){
+    this.current = 0;
+
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
+
+  app.controller("ReviewController", function(){
+
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+  var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
       shine: 8,
@@ -48,8 +59,7 @@
         author: "tim@example.org",
         createdOn: 1397490980837
       }]
-    },
-    {
+    }, {
       name: 'Bloodstone',
       description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
       shine: 9,
@@ -73,8 +83,7 @@
         author: "gemsRock@example.org",
         createdOn: 1397490980837
       }]
-    },
-    {
+    }, {
       name: 'Zircon',
       description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
       shine: 70,
@@ -85,7 +94,7 @@
       images: [
         "images/gem-06.gif",
         "images/gem-07.gif",
-        "images/gem-09.gif"
+        "images/gem-08.gif"
       ],
       reviews: [{
         stars: 1,
@@ -103,6 +112,5 @@
         author: "nat@example.org",
         createdOn: 1397490980837
       }]
-    }
-  ];
+    }];
 })();
