39d0bed2111f4985abcec2747fd3d3f647030325
Steven Mirecki <steven.mirecki@gmail.com>
2014-06-12 19:37:59 -0400

    further MongoDB Node additions
    Further MongoDB Node additions to build Restful API.

--- Node/nodecellar/server.js
+++ Node/nodecellar/server.js
@@ -5,8 +5,17 @@ var express = require('express'),
 
 var app = express();
 
+app.configure(function() {
+	app.use(express.logger('dev')); // 'default', 'short', 'tiny', 'dev'
+	app.use(express.bodyParser());
+});
+
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
+
 
 app.listen(3000);
 console.log('Listen on port 3000...');
