999bb3920df45dca4768e471d7c5555ff70edb35
Steven Mirecki <steven.mirecki@gmail.com>
2014-06-12 17:10:10 -0400

    Express install and config.
    Installing and Configuring Express.

---
+++ Node/nodecellar/routes/wines.js
@@ -0,0 +1,9 @@
+// wines.js
+
+exports.findAll = function(req, res) {
+	res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
+};
+
+exports.findById = function(req, res) {
+	res.send({id:req.params.id, name: "The Name", description: "description"});
+};
--- Node/nodecellar/server.js
+++ Node/nodecellar/server.js
@@ -1,8 +1,12 @@
 //server.js
 
-var http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express'),
+	wines = require('./routes/wines');
+
+var app = express();
+
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
+
+app.listen(3000);
+console.log('Listen on port 3000...');
