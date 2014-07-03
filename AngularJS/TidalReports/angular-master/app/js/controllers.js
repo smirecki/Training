affd747649a83213194ca624230becc9f3e63786
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-03 19:34:34 -0400

    Json
    Working locate json scoping.

--- AngularJS/TidalReports/angular-master/app/js/controllers.js
+++ AngularJS/TidalReports/angular-master/app/js/controllers.js
@@ -1,15 +1,10 @@
-'use strict';
-
-/* Controllers */
-
 var tidalReportApp = angular.module('tidalReportApp', []);
 
 
-  tidalReportApp.controller('TideListCtrl', function ($scope) {
-  	$scope.locations = [
-		{"name": "San Diego",   "high": "2014-06-23T08:25:43.511Z", "low": "2014-06-23T20:20:43.511Z"},
-		{"name": "Los Angeles", "high": "2014-06-23T08:10:00.511Z", "low": "2014-06-23T20:10:00.511Z"},
-		{"name": "Miami",       "high": "2014-06-23T11:00:00.511Z", "low": "2014-06-23T23:00:00.511Z"},
-		{"name": "New York",    "high": "2014-06-23T11:25:43.511Z", "low": "2014-06-23T23:25:43.511Z"}
-	];
+  tidalReportApp.controller('TideListCtrl', function ($scope, $http) {
+  	$http.get('locations.json').success(function(data){
+  		$scope.locations = data;
+  	});
+
+  	$scope.orderProp = 'name';
   });
