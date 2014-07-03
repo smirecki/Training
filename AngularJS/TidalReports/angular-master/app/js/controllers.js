b624348bf9f0955e3b1a524b8574dcc1a4c0f4db
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-03 17:34:27 -0400

    Working ng-repeat
    A lot of commented out Angular-seed structure, but basics are working.

--- AngularJS/TidalReports/angular-master/app/js/controllers.js
+++ AngularJS/TidalReports/angular-master/app/js/controllers.js
@@ -2,10 +2,14 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+var tidalReportApp = angular.module('tidalReportApp', []);
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+  tidalReportApp.controller('TideListCtrl', function ($scope) {
+  	$scope.locations = [
+		{"name": "San Diego",   "high": "2014-06-23T08:25:43.511Z", "low": "2014-06-23T20:20:43.511Z"},
+		{"name": "Los Angeles", "high": "2014-06-23T08:10:00.511Z", "low": "2014-06-23T20:10:00.511Z"},
+		{"name": "Miami",       "high": "2014-06-23T11:00:00.511Z", "low": "2014-06-23T23:00:00.511Z"},
+		{"name": "New York",    "high": "2014-06-23T11:25:43.511Z", "low": "2014-06-23T23:25:43.511Z"}
+	];
+  });
