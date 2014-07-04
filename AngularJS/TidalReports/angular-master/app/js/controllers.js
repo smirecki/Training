a0b0c35d0c5b3ba318145bec73d6cee02ed94ee0
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-04 19:10:12 -0400

    Restfull API

--- AngularJS/TidalReports/angular-master/app/js/controllers.js
+++ AngularJS/TidalReports/angular-master/app/js/controllers.js
@@ -1,8 +1,8 @@
 var tidalReportApp = angular.module('tidalReportApp', []);
 
-
   tidalReportApp.controller('TideListCtrl', function ($scope, $http) {
-  	$http.get('locations.json').success(function(data){
+
+  	$http.get('locations/location.json').success(function(data){
   		$scope.locations = data;
   	});
 
