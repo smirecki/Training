c7fdee20bf888622c4dfd31aea91112182b0e622
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-08 20:44:41 -0400

    Last update
    This example is out of date and conflicts with current Twitter API/Key
    rules and requirements.

--- AngularJS/TwitterJSONP/scripts/app.js
+++ AngularJS/TwitterJSONP/scripts/app.js
@@ -1,1 +1,12 @@
-'use strict';
+
+var app = angular.module("MyApp", ["ngResource"]);
+
+function MyCtrl($scope, $resource) {
+	$scope.twitterAPI = $resource("http://api.twitter.com/1.1/search/tweets.json",
+	  { callback: "JSON_CALLBACK" },
+	  { get: { method: "JSONP" }});
+
+	$scope.search = function() {
+		$scope.searchResult = $scope.twitterAPI.get({ q: $scope.searchTerm});
+	};
+}
