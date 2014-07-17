e195f79c981b3ec36da25458a4032bb1c1e2f735
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-16 22:53:27 -0400

    Building API Service
    The start to building the API Service for NOAA calls.

--- AngularJS/TidalReports/angular-master/app/js/services.js
+++ AngularJS/TidalReports/angular-master/app/js/services.js
@@ -9,5 +9,23 @@ tidalReportServices.factory('Location', ['$resource',
 		return $resource('locations/:stationID.json', {}, {
 			query: {method:'GET', params:{stationID:'locations'}, isArray:true}
 		});
-	}]);
 
+/*  Temp --- Building API Call Service
+tidalReportServices.factory('NoaaAPI', ['$resource',
+	function($resource){
+		return $resource('http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=:BeginDate&end_date=:EndDate&station=:StationNumber&product=:ProductType&units=UnitsLanguage&time_zone=:TimeZone&application=:ApplicationName&format=json', {}, {
+			query: {method:'GET', params:{
+				BeginDate :'',
+				EndDate :'',
+				StationNumber : '',
+				ProductType : '',
+				UnitsLanguage : 'english',
+				TimeZone : '',
+				ApplicationName : 'tidalReportApp'
+				
+
+
+		    }, isArray:true}
+		});
+*/
+	}]);
