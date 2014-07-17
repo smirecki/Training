84eafcbbdfe7e4ba6493ee2f3c109aac2282a1b5
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-17 11:02:26 -0400

    Cleaning up comments
    Removing unneeded commented out code.

--- AngularJS/TidalReports/angular-master/app/js/controllers.js
+++ AngularJS/TidalReports/angular-master/app/js/controllers.js
@@ -16,21 +16,3 @@ tidalReportControllers.controller('LocationDetailCtrl', ['$scope', '$routeParams
   		$scope.mainImageUrl = imageUrl;
   	}
   }]);
-
-/*
-tidalReportControllers.controller('LocationListCtrl', ['$scope', '$http',
-	function ($scope, $http) {
-	$http.get('locations/locations3.json').success(function(data) {
-		$scope.locations = data;
-	});
-
-}]);
-
-tidalReportControllers.controller('LocationDetailCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-     $http.get('locations/' + $routeParams.stationID + '.json').success(function(data) {
-      $scope.location = data;
-    });
-  }]);
-
-  */
