fecfc32057fd3d8f5992194220511ac2aa1b9cbd
Steven Mirecki <steven.mirecki@gmail.com>
2014-07-17 21:59:23 -0400

    Word filter
    Non-working start to a new word Filter, which isn't even needed at this
    point.

--- AngularJS/TidalReports/angular-master/app/js/filters.js
+++ AngularJS/TidalReports/angular-master/app/js/filters.js
@@ -2,9 +2,9 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
+angular.module('tidalReportApp.filters', []).
+  filter('wordLimit', ['version', function() {
+    return function(input) {
+      return;
     };
   }]);
