d7066153d8c6893f698a5d1be3a3ece3e4c5606b
Steven Mirecki <steven.mirecki@gmail.com>
2014-08-24 11:22:38 -0400

    Examples and notes on Date() in JS
    Examples and notes on Date() in JS

---
+++ AngularJS/TidalReports/angular-master/app/notes/time.js
@@ -0,0 +1,31 @@
+// Date()
+/*
+var start = Date.now();
+var start2 = new Date("December 25, 1995 23:15:00");
+
+var foo = start - 1;
+
+console.log(foo);
+
+
+var bar = start.getDate();
+*/
+
+var d = new Date();
+console.log(d.getTime);
+
+/*
+http://jsfiddle.net/jfriend00/NYSsp/
+
+var prevTime = new Date(2014,8,1,0,0);  // Aug 1, 2014
+var thisTime = new Date();              // now
+var diff = thisTime.getTime() - prevTime.getTime();   // now - Aug 1
+alert(diff / (1000*60*60*24));     // positive number of days
+
+http://jsfiddle.net/w5tpncxd/
+
+var a = new Date(2011,7,5,2,1,1) - new Date(2011,7,5,1,1,1);
+a = a/3600000; // 3600000 which is millisecond for 1 hour. Divide by this to get hours.
+alert(a);
+
+*/
