6336a5825e0a7b3c96c5c3c6c8bb7159c497379e
Steven Mirecki <steven.mirecki@gmail.com>
2014-08-27 21:49:29 -0400

    update
    update

--- AngularJS/TidalReports/angular-master/app/notes/time.js
+++ AngularJS/TidalReports/angular-master/app/notes/time.js
@@ -25,7 +25,34 @@ alert(diff / (1000*60*60*24));     // positive number of days
 http://jsfiddle.net/w5tpncxd/
 http://jsfiddle.net/w5tpncxd/1/
 http://jsfiddle.net/w5tpncxd/2/
+=======================
+//var a = 2 - 1;
+var a = new Date();
+var b = new Date(2014,7,24,10,28,1);
+var c = a - b;
+var d = b + 22350000;
+var year = 2014;
+var month = 8;
+var day = 1;
+var hour = 22;
+var minute = 45;
+var e = new Date(year, month, day, hour, minute);
 
+//foo = 3600000 * 6.25
+//a = a/360000; // 3600000 which is millisecond for 1 hour. Divide by this to get hours.
+console.log(a);
+console.log(b);
+console.log(b + 22350000);
+console.log(c);
+console.log(c/60000)
+console.log(d);
+console.log(e);
+//alert(c);
+//alert(new Date());
+
+//14:58
+
+=======================
 var a = new Date(2011,7,5,2,1,1) - new Date(2011,7,5,1,1,1);
 a = a/3600000; // 3600000 which is millisecond for 1 hour. Divide by this to get hours.
 alert(a);
