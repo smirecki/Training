9a50afdff7901f1cd704a0dd673fee644c2443d6
Steven Mirecki <steven.mirecki@gmail.com>
2014-11-18 19:19:57 -0500

    update spider
    using item

--- Scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ Scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -10,8 +10,9 @@ class DmozSpider(scrapy.Spider):
 
     def parse(self, response):
         for sel in response.xpath('//ul/li'):
-            title = sel.xpath('a/text()').extract()
-            link = sel.xpath('a/@href').extract()
-            desc = sel.xpath('text()').extract()
-            print title, link, desc
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
  
