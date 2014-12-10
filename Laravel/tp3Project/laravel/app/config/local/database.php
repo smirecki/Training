123d8373270d7fdf5b39ff1899ea3363cdb85e99
Steven Mirecki <steven.mirecki@gmail.com>
2014-12-10 16:10:42 -0500

    db config edit mysql
    Local mysql config

--- Laravel/tp3Project/laravel/app/config/local/database.php
+++ Laravel/tp3Project/laravel/app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'root',
-			'password'  => 'secret',
+			'database'  => 'laravel',
+			'username'  => 'steve',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
