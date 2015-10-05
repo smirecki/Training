9e0e149d0a2831d82b0bb0e4a263cd0f57458300
smirecki <steven.mirecki@gmail.com>
2015-10-05 17:48:56 -0400

    Update finish 0206
    Finished 0206, focus on Object template usage and @data variables.

--- Handlebars/handlebars-app/js/app.js
+++ Handlebars/handlebars-app/js/app.js
@@ -1,14 +1,24 @@
 (function() {
   
   renderPage();
-
+  renderDogs();
+  
   function renderPage() {
     var template = $('#index-template').html(),
       compiled = Handlebars.compile(template),
       rendered = compiled(window.language);
     $('#main').html(rendered);
+
     $('#languageSwitch').click(function() {
         DogPack.switchLanguage();
     });
+  }
+
+
+  function renderDogs() {
+    var template = $('#dogs-template').html(),
+        compiled = Handlebars.compile(template),
+        rendered = compiled({ dogs: DogPack.dogs, language: window.language });
+    $('#theDogs').html(rendered);
   }
 })();
