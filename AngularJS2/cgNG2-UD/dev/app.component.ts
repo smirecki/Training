5fc5d1da607c820aac5c3665e9d631516cc2f485
steven mirecki <steven.mirecki@gmail.com>
2016-03-15 19:24:42 -0400

    Prep folder for start of section 4
    Removed files and folders for prep of section 4.

--- AngularJS2/cgNG2-UD/dev/app.component.ts
+++ AngularJS2/cgNG2-UD/dev/app.component.ts
@@ -1,28 +1,3 @@
 import {Component} from 'angular2/core';
-import {InputComponent} from "./bindings/input.component";
-import {ConfirmComponent} from "./bindings/confirm.component";
 
-@Component({
-    selector: 'my-app',
-    template:` 
-        <div class="container">
-          <my-input (submitted)="onSubmit($event)" [myself]="confiredMyself"></my-input>
-        </div>
-        <div class="container">
-          <my-confirm (confirmed)="onConfirm($event)" [myself]="myself"></my-confirm>
-        </div>
-    `,
-    directives: [InputComponent, ConfirmComponent]
-})
-export class AppComponent {
-    myself = {name: '', age: ''};
-    confirmedMyself = {name: '', age: ''};
-    
-    onSubmit(myself: {name: string, age: string}) {
-        this.myself = {name: myself.name, age: myself.age};  
-    }
-    
-    onConfirm(myself: {name: string, age: string}) {
-        this.confirmedMyself = {name: myself.name, age: myself.age};
-    }
-}
+
