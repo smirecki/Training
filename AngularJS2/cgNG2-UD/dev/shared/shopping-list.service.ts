b8ed4f4cc773377904751488ebf62414df3818f1
steven mirecki <steven.mirecki@gmail.com>
2016-03-24 19:24:46 -0400

    Update Folder Structure Shared Elements
    Added a few files.

---
+++ AngularJS2/cgNG2-UD/dev/shared/shopping-list.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from 'angular2/core';
+import {Ingredient} from "./ingredient";
+
+@Injectable()
+export class ShoppingListService {
+    getAllItems() {
+        
+    }
+    
+    getItem(index: number) {
+        
+    }
+    
+    getIndexOfItem(item: Ingredient) {
+        
+    }
+    
+    insertItem(item: Ingredient) {
+        
+    }
+    
+    InsertItems(items: Ingredient[]) {
+        
+    }
+    
+    deleteItem(item: Ingredient) {
+        
+    }
+    
+    updateItem(index: number, item: Ingredient) {
+        
+    }
+}
