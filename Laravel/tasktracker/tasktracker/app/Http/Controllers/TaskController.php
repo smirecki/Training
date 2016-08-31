be61390d73df95d3a91f66a8a89c9085c16384ec
steven mirecki <steven.mirecki@gmail.com>
2016-08-31 19:48:34 -0400

    Update TaskController for index route

--- Laravel/tasktracker/tasktracker/app/Http/Controllers/TaskController.php
+++ Laravel/tasktracker/tasktracker/app/Http/Controllers/TaskController.php
@@ -5,8 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Task;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+
+	/**
+	 * Display a list of all of the user's task.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function index(Request $request)
+	{
+	    return view('tasks.index');
+	}
+
 }
