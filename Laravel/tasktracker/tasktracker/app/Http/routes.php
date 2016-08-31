9361706a98935f788b093359423e16b691b3a3df
steven mirecki <steven.mirecki@gmail.com>
2016-08-31 19:49:25 -0400

    update the routes further to build propert auth function with users

--- Laravel/tasktracker/tasktracker/app/Http/routes.php
+++ Laravel/tasktracker/tasktracker/app/Http/routes.php
@@ -7,3 +7,8 @@ use Illuminate\Http\Request;
 // Authentication Routes...
 Route::auth();
 
+
+// "Stub(bed) out routes", to be changed... are these behind the auth routes yet???
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
