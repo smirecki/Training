c70174624e547f7fa8c0ddd4cb1320087d61e508
steven mirecki <steven.mirecki@gmail.com>
2016-08-30 20:30:12 -0400

    Create vaildator in route for task

--- Laravel/tasktracker/tasktracker/app/Http/routes.php
+++ Laravel/tasktracker/tasktracker/app/Http/routes.php
@@ -30,7 +30,17 @@ Route::get('/', function () {
  * Add New Task
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Create The Task...
 });
 
 /**
